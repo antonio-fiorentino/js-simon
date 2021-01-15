adc5b81ab2b478e1734417b7b888b66bab86c1ee
antonio-fiorentino <antoniofiorentino16@gmail.com>
2021-01-15 17:32:12 +0100

    Esercizio

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+//Genera numeri random
 function randomNumber(min, max) {
   var result = Math.floor(Math.random() * (max + 1 - min) + min);
   return result;
@@ -6,7 +6,7 @@ function randomNumber(min, max) {
 
 var numeriPc = [];
 var numeriUtente = [];
-
+//Verifica se non ci sono numeri uguali
 while (numeriPc.length<5) {
   var random = randomNumber(1, 100);
   console.log(random);
@@ -20,6 +20,7 @@ console.log(numeriPc);
 var alertNumeri = alert(numeriPc);
 
 
+//countdown
 var element = document.getElementById('conto');
 var conto = 3;
 
@@ -30,7 +31,7 @@ var countdown = setInterval(function() {
   conto--;
   element.innerText = conto
   if(conto === 0) {
-
+//5 numeri inseriti dall'utente
     setTimeout(function() {
       for (var i = 0; i < 5; i++) {
         var utente = parseInt(prompt('Inserire i numeri'));
@@ -40,8 +41,16 @@ var countdown = setInterval(function() {
         }else {
           numeriUtente.push(utente);
           console.log('Numeri utente ' + numeriUtente);
+        }}
+        //Risultato
+        var numeriCorretti = parseInt(0);
+
+        if (numeriPc.includes(numeriUtente)=== true) {
+          var risultato = numeriCorretti + 1
         }
-        }
+
+        console.log('I numeri corretti sono: '+ risultato);
+
     }, 200);
 
     clearInterval(countdown)
